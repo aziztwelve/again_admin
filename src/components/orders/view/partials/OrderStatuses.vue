246f74179dbaa4958a9958938ec427f927affcf8
Aziz Karimov <aziztwelve@gmail.com>
2026-09-05 03:47:10 +0300

    Align refund button left with paid date field

--- src/components/orders/view/partials/OrderStatuses.vue
+++ src/components/orders/view/partials/OrderStatuses.vue
@@ -91,7 +91,7 @@
       </div>
 
       <!-- Возврат оплаты: клиент отказался от покупки после оплаты -->
-      <div v-if="canRefundPayment" class="col-span-2 pt-8">
+      <div v-if="canRefundPayment" class="col-span-2 lg:col-start-1">
         <Button
           type="button"
           variant="destructive"
